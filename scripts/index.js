12ac76a67588a43bbaed8cdf712b2fe9fb4df20b
Deacon Griffiths <134710005+dj4christ@users.noreply.github.com>
2024-10-07 17:58:39 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const passwordInput = document.getElementById("passwordInput");
 goButton.addEventListener("click", function (){
     if(usernameInput.value == "Abigail Griffiths") {
         if(passwordInput.value == "1232589875") {
-            window.location.replace("mainGame.html")
+            window.location.replace("../public/mainGame.html")
         } else {
             alert("Password is incorrect. Try again.")
         }
